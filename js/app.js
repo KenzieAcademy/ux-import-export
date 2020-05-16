364554f8f819b0ba91f66c480c60b06cc8972bf4
jeann9ne <jeanninecoons@gmail.com>
2020-05-16 15:38:23 -0400

    all done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 import { Person, render } from "./personalProfile.js"
 
 let people = [
-    new Person("Bob", "bob-ross.jpg"),
-    new Person("Frida", "frida-kahlo.jpg"),
-    new Person("Dali", "dali.jpg")
+    new Person("Bob", "./image/bob-ross.jpg"),
+    new Person("Frida", "./image/frida-kahlo.jpg"),
+    new Person("Dali", "./image/dali.jpg")
 ];
 
 window.onload = () => {
