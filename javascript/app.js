e07359d9b3d1daabdf9a989ba07f8342f9ccc785
fmsieck <fmsieck@gmail.com>
2020-09-08 14:19:43 -0400

    first commit. after a plethora of problems, this was finally figured out. as per usual, i was making things much more harder than they needed to be. with a little direction from izzy and megan, i was able to figure out what i was doing incorrectly to get the images to show.

---
+++ javascript/app.js
@@ -0,0 +1,26 @@
+import { Person, render } from "./personalProfile.js";
+// import Person from "../images";
+
+let people = [
+  new Person("Bob", "./images/bob-ross.jpg"),
+  new Person("Frida", "./images/frida-kahlo.jpg"),
+  new Person("Dali", "./images/dali.jpg"),
+];
+
+window.onload = () => {
+  renderList(people);
+};
+
+function renderList(people) {
+  let html = "";
+  html += `<ul>`;
+  for (let person of people) {
+    html += "<li>";
+    html += render(person);
+    html += "</li>";
+  }
+  html += "</ul>";
+
+  document.getElementById("list").innerHTML = html;
+}
+renderList(people);
